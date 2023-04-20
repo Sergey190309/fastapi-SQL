c60677c503564a0300dd44ee127d483a9e3cb532
Sergey <sa6702@gmail.com>
2023-04-20 07:11:24 +0100

    it seems I have made acync db operations

--- api/sqlalchemy_models/models.py
+++ api/sqlalchemy_models/models.py
@@ -1,30 +1,32 @@
+from typing import List
 from sqlalchemy import (
-    Boolean, Column,
+    # Boolean, Column,
     ForeignKey,
-    Integer, String
+    # Integer, String
     )
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from ..db.init_db import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users_table"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column()
+    is_active: Mapped[bool] = mapped_column(default=True)
 
-    items = relationship("Item", back_populates="owner")
+    items: Mapped[List['Item']] = relationship(back_populates='owner')
+    # items = relationship("Item", back_populates="users")
 
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "items_table"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(index=True)
+    description: Mapped[str] = mapped_column(index=True)
+    owner_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
-    owner = relationship("User", back_populates="items")
+    owner: Mapped['User'] = relationship(back_populates="items")
